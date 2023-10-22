323be6fbb6c0184dbf14698c85206da6738203a0
kalpik1597 <148410715+kalpik1597@users.noreply.github.com>
2023-10-22 17:18:58 +0530

    update storage classes

--- storage_classes/program_1.c
+++ storage_classes/program_1.c
@@ -2,6 +2,8 @@
 
 int int_outside_main;
 void function_in_another_file();
+void function_in_same_file();
+extern int int_in_another_file_two;
 
 int main() {
 
@@ -14,10 +16,16 @@ int main() {
     printf("static_int_in_main = %d\n",static_int_in_main);
     function_in_another_file();
     extern int int_in_another_file;
+    int_in_another_file = -55;
     printf("extern_int_from_another_file = %d\n",int_in_another_file);
-    //extern int int_in_another_file_in_fuction;
-    //printf("int_in_another_file = %d\n",int_in_another_file_in_fuction);
+    function_in_same_file();
 
     printf("==== program_end ====\n");
     return 0;
+}
+
+void function_in_same_file(){
+    register int register_int;
+    printf("int_from_another_file_two = %d\n",int_in_another_file_two);
+    printf("register_int = %d\n",register_int);
 }
--- storage_classes/program_2.c
+++ storage_classes/program_2.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 
 int int_in_another_file = 108;
+int int_in_another_file_two = 111;
+static int static_int_in_another_file = -110;
 
 void function_in_another_file() {
 
