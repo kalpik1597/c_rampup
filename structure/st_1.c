2f5b982ce408f4e0fe907541ceac0c2c46808bd5
kalpik1597 <148410715+kalpik1597@users.noreply.github.com>
2023-10-29 17:13:18 +0530

    Update st_1.c

--- structure/st_1.c
+++ structure/st_1.c
@@ -15,13 +15,19 @@ struct st_1{
 
 void main(){
     struct st_1 st_1_var_1;
+    struct st_1 st_1_var_2;
     st_1_var_1.i = 10;
     st_1_var_1.ch[0] = 'k';
     st_1_var_1.f = 25.5;
     st_1_var_1.a.ch[0] = 'p';
+    st_1_var_2=st_1_var_1;
     printf("st_1_var_1.i: %d\n",st_1_var_1.i);
     printf("st_1_var_1.ch[0]: %c\n",st_1_var_1.ch[0]);
     printf("st_1_var_1.f: %f\n",st_1_var_1.f);
-    printf("%c from st_2\n",st_1_var_1.a.ch[0]);
+    printf("st_1_var_1.a.ch[0]: %c\n",st_1_var_1.a.ch[0]);
+    printf("st_1_var_2.i: %d\n",st_1_var_2.i);
+    printf("st_1_var_2.ch[0]: %c\n",st_1_var_2.ch[0]);
+    printf("st_1_var_2.f: %f\n",st_1_var_2.f);
+    printf("st_1_var_2.a.ch[0]: %c\n",st_1_var_2.a.ch[0]);
     
 }
